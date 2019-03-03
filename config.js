aa23c41f2080ea46663c2f46d849bbb941834d30
julio montilla <juliomontilla100@gmail.com>
2019-03-03 20:51:03 -0300

    deploy

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ process.env.PORT = process.env.PORT || 3000
 if(process.env.NODE_ENV === 'dev'){
     process.env.URL_DB = 'mongodb://localhost:27017/maratones'
 }else {
-    process.env.URL_DB = 'mongodb+srv://juliomontilla100:04186131441@marathon-julion-db-urtk0.mongodb.net/test?retryWrites=true'
+    process.env.URL_DB = 'mongodb+srv://juliomontilla100:04186131441@marathons-apirest-urtk0.mongodb.net/test?retryWrites=true'
 }
